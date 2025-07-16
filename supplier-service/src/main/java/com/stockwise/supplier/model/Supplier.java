1b34182affd8df5d54d4b94714082d86ed1f139d
TheTiscks <s-kretov@mail.ru>
2025-07-16 21:59:08 +0300

    Supplier model full realisation

--- supplier-service/src/main/java/com/stockwise/supplier/model/Supplier.java
+++ supplier-service/src/main/java/com/stockwise/supplier/model/Supplier.java
@@ -1,20 +1,115 @@
-package com.stockwise.inventory.model;
+package com.stockwise.supplier.model;
 
 import jakarta.persistence.*;
-import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
-public class InventoryItem {
+public class Supplier {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long inventoryId;
+    private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "product_id")
-    private Product product;
+    private String name;
+    private String contactEmail;
+    private String phoneNumber;
+    private String address;
 
-    private int quantity;
-    private Instant lastUpdated = Instant.now();
+    @OneToMany(
+            mappedBy = "supplier",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Contract> contracts = new ArrayList<>();
 
-    // Конструкторы, геттеры, сеттеры
+    public Supplier() {
+    }
+
+    public Supplier(String name, String contactEmail, String phoneNumber, String address) {
+        this.name = name;
+        this.contactEmail = contactEmail;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Contract> getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(List<Contract> contracts) {
+        this.contracts = contracts;
+    }
+
+    public void addContract(Contract contract) {
+        contracts.add(contract);
+        contract.setSupplier(this);
+    }
+
+    public void removeContract(Contract contract) {
+        contracts.remove(contract);
+        contract.setSupplier(null);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Supplier supplier = (Supplier) o;
+        return Objects.equals(id, supplier.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", contactEmail='" + contactEmail + '\'' +
+                '}';
+    }
 }
